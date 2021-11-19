67b33ef3ad98e1e61a04573dafc7fe38016d3e9b
lvshihao.nb <lvshihao.nb@bytedance.com>
2021-11-19 19:51:57 +0800

    feat: export types

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { DragDropContext } from "./contexts";
 import { Ghost, Draggable, Droppable, DragDropProvider } from "./components";
 
+export * from "./types";
 export { Ghost, Draggable, Droppable, DragDropContext, DragDropProvider };
 
 export default {
